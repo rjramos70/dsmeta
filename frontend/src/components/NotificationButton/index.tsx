8898a239c719782a0d66ea06346f647f3ec75458
Renato Ramos <rramos.gft@alelo.com.br>
2022-11-18 19:07:38 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,23 @@
+import axios from 'axios';
 import bt_notification from '../../assets/img/notification-icon.svg'
+import { BASE_URL } from '../../utils/request';
 
 import './styles.css'
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id : number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+        console.log("SUCESSO");
+    })
+}
+
+function NotificationButton( {saleId} : Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn"n onClick={() => handleClick(saleId)}>
             <img src={bt_notification} alt="Notificar" />
         </div>    
     )
