66497bcdc98347ef0a4e7e349494c72f061394b9
Renato Ramos <rramos.gft@alelo.com.br>
2022-11-18 22:11:00 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import bt_notification from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request';
 
@@ -11,13 +12,13 @@ type Props = {
 function handleClick(id : number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response => {
-        console.log("SUCESSO");
+        toast.info(`SMS enviado com sucesso para id ${id}`);
     })
 }
 
 function NotificationButton( {saleId} : Props) {
     return (
-        <div className="dsmeta-red-btn"n onClick={() => handleClick(saleId)}>
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={bt_notification} alt="Notificar" />
         </div>    
     )
